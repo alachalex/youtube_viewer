ef22c0dd8b044e7119cdc0c7a9e00375b5dcf5ad
alachalex <alachalex16@gmail.com>
2020-10-25 21:18:57 -0400

    Update jsfile.js

--- jsfile.js
+++ jsfile.js
@@ -2,7 +2,8 @@ $(document).ready(function(){
     // declaring some variables including the video URL and the space
     // where videos will show on the HTML
     var link = ''
-
+    //console.log("im going to kill myself");
+    
     //This function takes the submission
     $("#searching").submit(function (event) {
         event.preventDefault()
@@ -13,13 +14,15 @@ $(document).ready(function(){
     //this function gets the results of the submission from the 
     // google api website and displays in the HTML
     function videofind(query){
-        $.get("https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyAAOOC0KOMi0QniEruh91kTnRnQTWWkVLs&type=video&q=" + query, function(data){
-            console.log(data)
+        console.log("im going to kill myself")
+        $.get("https://www.googleapis.com/youtube/v3/search?key=AIzaSyAAOOC0KOMi0QniEruh91kTnRnQTWWkVLs&type=video&part=snippet&q=" + query, function(data){
+            console.log("here is data",data);
                 data.items.forEach(item =>{
-                    link =  <iframe width = "600" height = "550" src = "http://www.youtube.com/embed/${item.id.videoId}" frameboarder="0"></iframe>
+                    link =  <iframe width = "600" height = "550" src = "http://www.youtube.com/embed/${item.id.videoId}" frameboarder="0" allowfullscreen></iframe>
                     $("results").append(link)
                 });
         })
     }
-}
+    
+})
 
